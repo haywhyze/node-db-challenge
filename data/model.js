22c8a36a958341c6b6c3a3625dc33bdbebed9ed3
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-19 10:26:16 +0100

    feat: create data model helpers to query db

---
+++ data/model.js
@@ -0,0 +1,20 @@
+const db = require('./dbConfig');
+
+async function getProjectById(id) {
+  const project = await db('projects')
+    .where('id', id)
+    .first();
+  if (project) project.completed = !!project.completed;
+  if (project) project.actions = await db('actions').where('project_id', id);
+  if (project.actions) {
+    project.actions.forEach((action) => {
+      // eslint-disable-next-line no-param-reassign
+      action.completed = !!action.completed;
+    });
+  }
+  return project;
+}
+
+module.exports = {
+  getProjectById,
+};
