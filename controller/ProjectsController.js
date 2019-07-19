9bc9084ffa6727e3cfefeda3dded2b70c36fe090
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-19 11:12:08 +0100

    feat: create project controller

--- controller/ProjectsController.js
+++ controller/ProjectsController.js
@@ -1,5 +1,19 @@
+const db = require('../data/model');
 
 class Projects {
+  static async create(req, res, next) {
+    const newProject = {
+      name: req.body.name,
+      description: req.body.description,
+    };
+    try {
+      const newProjectData = await db.insertProject(newProject);
+      return res.status(201).json(newProjectData);
+    } catch (error) {
+      return next(error);
+    }
+  }
+
   static async getOne(req, res) {
     return res.status(200).send(req.project);
   }
