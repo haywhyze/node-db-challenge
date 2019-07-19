7fad72a1260b49a0b072c12f37fa5dd0a8817b04
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-19 12:09:40 +0100

    feat: add post actions to route

--- routes/projects.js
+++ routes/projects.js
@@ -6,5 +6,6 @@ const projects = express.Router();
 
 projects.get('/:id', middlewares.validateId, ProjectsController.getOne);
 projects.post('/', middlewares.validateProject, ProjectsController.create);
+projects.post('/:id/actions', middlewares.validateId, middlewares.validateAction, ProjectsController.createAction);
 
 module.exports = projects;
