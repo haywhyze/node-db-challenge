56f9fd9e7f12be5ee999029649d5bbc931f23d83
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-19 10:46:59 +0100

    feat: add validateId middleware

---
+++ middlewares/index.js
@@ -0,0 +1,26 @@
+const db = require('../data/model');
+
+async function validateId(req, res, next) {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id) || id % 1 !== 0 || id < 0) {
+    return res.status(400).send({
+      message: 'invalid id provided',
+    });
+  }
+  try {
+    const data = await db.getProjectById(id);
+    if (!data) {
+      return res.status(404).send({
+        message: 'id provided does not exist',
+      });
+    }
+    req.project = data;
+  } catch (error) {
+    return next(error);
+  }
+  return next();
+}
+
+module.exports = {
+  validateId,
+};
